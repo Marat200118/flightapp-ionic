6c85e8493ec2927afb0d1965c91fbc0dd7c35359
Marat200118 <marat.samigullin1@gmail.com>
2025-01-07 15:43:35 +0100

    fixed the integration with xcode, so I can use it

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'flightapp.ionic',
-  appName: 'flightapp-ionic',
+  appId: 'io.ionic.starter',
+  appName: 'ionic-app-base',
   webDir: 'www'
 };
 
